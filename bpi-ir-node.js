337e958288f47f2bf9786f453d35aa38d3703bd9
Adrian Sławiński <adrian.slawinski@javadevelopers.pl>
2015-08-28 10:36:42 +0200

    update ir module

--- bpi-ir-node.js
+++ bpi-ir-node.js
@@ -1,24 +1,37 @@
-var ir = require('node-ir');
 module.exports = function(RED) {
-    
+    var IR = require('bpi-ir');
     function BpiIrOutNode(config) {
+        var ir = new IR();
         RED.nodes.createNode(this,config);
         this.debug = config.debug || false;
-        this.keyMap = config.keyMap || {};
+        this.keyMap = config.keymap || {};
         var node = this;
-        ir.displayEvents();
-        ir.on('key', function(key) {
+        ir.start();
+	var lastKey = null;
+        var mappKey = function(key) {
             var mappedKey = node.keyMap[key];
             if (mappedKey == undefined) {
                 mappedKey = key;
             }
+            return mappedKey;
+        }
+	ir.on('down', function(key) {
+            var mappedKey = mappKey(key);
             var msg = { payload: mappedKey };
-            node.send(msg);
-            if (node.debug) {
+            if (node.debug) {   
                 node.status({fill:"green",shape:"ring",text:"Key: " + mappedKey});
             }
+            node.send([msg, null]);
+	});
+        ir.on('up', function(key) {
+            var mappedKey = mappKey(key);
+            var msg = { payload: mappedKey };
+            node.send([null, msg]);
+            if (node.debug) {
+                node.status({fill:"red",shape:"ring",text:"Key: " + mappedKey});
+            }
         });
-        this.on('close', ir.hideEvents);
+        this.on('close', ir.stop);
     }
-    RED.nodes.registerType("banana-ir out", BpiIrOutNode);
+    RED.nodes.registerType("banana-ir in", BpiIrOutNode);
 }
